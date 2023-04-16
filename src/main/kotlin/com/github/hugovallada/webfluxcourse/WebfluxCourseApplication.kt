9a765c2d35eb59b7e32d12abb39f2c334a53b86c
Hugo Vallada <valladahugo@gmail.com>
2023-04-16 16:04:54 -0300

    Configuração inicial do projeto

---
+++ src/main/kotlin/com/github/hugovallada/webfluxcourse/WebfluxCourseApplication.kt
@@ -0,0 +1,11 @@
+package com.github.hugovallada.webfluxcourse
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebfluxCourseApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebfluxCourseApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/hugovallada/webfluxcourse/WebfluxCourseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.hugovallada.webfluxcourse
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebfluxCourseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
